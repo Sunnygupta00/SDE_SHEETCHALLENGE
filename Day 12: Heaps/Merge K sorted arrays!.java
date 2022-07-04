c62a10c683e24a944d2af445feafebc365bfff3c
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2022-07-04 12:09:39 +0530

    Update Merge K sorted arrays!.java

--- Day 12: Heaps/Merge K sorted arrays!.java
+++ Day 12: Heaps/Merge K sorted arrays!.java
@@ -1,41 +1,23 @@
 public class Solution {
-    public ArrayList<Integer> solve(ArrayList<Integer> A, ArrayList<Integer> B, int C) {
-        Collections.sort(A, Collections.reverseOrder());
-        Collections.sort(B, Collections.reverseOrder());
-        PriorityQueue<Integer> pq = new PriorityQueue();
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        for(int i=0; i<A.size();i++)
-        {
-            for(int j=0;j< B.size(); j++)
-            {
-               int sum= A.get(i)+B.get(j);
-               if(pq.size()<C)
-               {
-                   pq.add(sum);
-               }
-               else 
-               {
-                   if(sum>pq.peek())
-                   {
-                       pq.poll();
-                       pq.add(sum);
-                   }
-                   else if(sum<pq.peek())
-                   {
-                      break; 
-                   }
-               }
+    public ArrayList<Integer> solve(ArrayList<ArrayList<Integer>> a) {
+        Queue<ArrayList<Integer>> pq = new PriorityQueue<>(new Comparator<ArrayList<Integer>>() {
+            @Override
+            public int compare(ArrayList<Integer> a, ArrayList<Integer> b) {
+                return a.get(0) - b.get(0);
+            }
+        });
+    
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(ArrayList<Integer> l: a){
+            pq.offer(l);
+        }
+        while(!pq.isEmpty()){
+            ArrayList<Integer> curr = pq.poll();
+            list.add(curr.get(0));
+            if(curr.size()>1){
+                pq.offer(new ArrayList<Integer>(curr.subList(1,curr.size())));
             }
         }
-       
-       while(!pq.isEmpty())
-       {
-           arr.add(pq.peek());
-           pq.poll();
-       }
-       Collections.sort(arr, Collections.reverseOrder());
-       
-       return arr;
+        return list;
     }
 }
-
