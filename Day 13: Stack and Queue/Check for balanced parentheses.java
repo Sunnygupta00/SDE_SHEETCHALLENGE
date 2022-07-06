35c8c1411de7a4c30535a3789d76e895dd21513b
SUNNY GUPTA <80736254+Sunnygupta00@users.noreply.github.com>
2022-07-07 00:01:38 +0530

    Update Check for balanced parentheses.java

--- Day 13: Stack and Queue/Check for balanced parentheses.java
+++ Day 13: Stack and Queue/Check for balanced parentheses.java
@@ -1,1 +1,25 @@
-// EMPTY
+class Solution {
+ public static boolean isValid(String s) {
+	Deque <Character>st=new ArrayDeque<Character>();
+	for(char c:s.toCharArray())  // INSERTING ONLY OPENING BRACKETS BECAUSE AT THE TIME OF CLOSING BRACKET WE CAN EASILY CHECK
+	{
+		if(c=='(')
+		{
+			st.push(')');
+		}
+		else if(c=='{')
+		{
+			st.push('}');
+		}
+		else if(c=='[')
+		{
+			st.push(']');
+		}
+		else if(st.isEmpty()||st.pop()!=c){ // IF NO VALUE IS THERE OR BOTH THE BRACKETS ARE NOT MATCHING RETURN FALSE;
+			return false;
+		}
+        
+	}
+	return st.isEmpty();
+}
+}
